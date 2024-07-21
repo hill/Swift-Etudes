83d0ea1de7aadc83c33d8697c2e4c9434fe805ff
Tom Hill <tomhill98@me.com>
2024-07-21 18:22:03 +1000

    Initial Commit

---
+++ Swift Lab/Swift_LabApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_LabApp.swift
+//  Swift Lab
+//
+//  Created by Tom Hill on 21/7/2024.
+//
+
+import SwiftUI
+
+@main
+struct Swift_LabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
